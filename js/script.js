56f5f6007b32e68d007032fef77af0c1a4eb7083
Nick <84956743974@mail.ru>
2022-10-15 22:38:25 +0300

    add js files, add clear button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 ﻿"use strict";
+(function() {
+    const createDataBase = {
+        clearForm(but) {
+            but.preventDefault();
+            
+            let allInputs = document.querySelectorAll(".form-control");
+            allInputs.forEach(item => item.value = "");
+        },
+    };
+    
+    let formClear = document.querySelector(".btn-danger");
+    formClear.addEventListener("click", createDataBase.clearForm);
+})();
