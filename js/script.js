76ad0d4458009b1abb7b39f4ff57861d807d2f93
Nick <84956743974@mail.ru>
2022-10-19 00:32:07 +0300

    continue working with table

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 (function() {
     const createDataBase = {
         arrayOfObjectsWithStudents: [], // глобальный массив, содержащий объекты всех студентов
-        numberOfStudent: 1,
 
         clearForm(but) {
             but.preventDefault();
@@ -19,8 +18,8 @@
             nowObject.surname = allInputs[0].value;
             nowObject.name = allInputs[1].value;
             nowObject.lastname = allInputs[2].value;
-            nowObject.birthDate = allInputs[3].value;
-            nowObject.startStudyDate = allInputs[4].value;
+            nowObject.birthDate = new Date(allInputs[3].value);
+            nowObject.startStudyYear = allInputs[4].value;
             nowObject.facultet = allInputs[5].value;
             
             this.arrayOfObjectsWithStudents.push(nowObject); // добавляем обхект в глобальный массив со студентами
@@ -29,29 +28,37 @@
 
         drawTableWithStudents(array) {
             let tbody = document.getElementById("tbody");
-            let tr = document.createElement("tr");
-
             tbody.innerHTML = '';
 
-            for (let element of array) {
+            for (let i = 1; i < array.length + 1; i++) {
+                let tr = document.createElement("tr");
                 let th = document.createElement("th");
                 let firstTd = document.createElement("td");
                 let secondTd = document.createElement("td");
                 let thirdTd = document.createElement("td");
                 let fourthTd = document.createElement("td");
+                let dateNow = new Date();
+                let yearsFromStartStudy = dateNow.getFullYear() - array[i - 1].startStudyYear;
+                let age = Math.trunc((dateNow.getTime() - array[i - 1].birthDate.getTime()) / 1000 / 60 / 60 / 24 / 365);
 
-                th.textContent = this.numberOfStudent;
+                
+                th.textContent = i;
                 th.setAttribute("scope", "row");
-                firstTd.textContent = element.surname + " " + element.name + " " + element.lastname;
-                secondTd.textContent = element.facultet;
 
+                firstTd.textContent = array[i - 1].surname + " " + array[i - 1].name + " " + array[i - 1].lastname;
+                secondTd.textContent = array[i - 1].facultet;
+                thirdTd.textContent = `${array[i - 1].birthDate.getDate()}.${array[i - 1].birthDate.getMonth() + 1}.${array[i - 1].birthDate.getFullYear()} (${age} лет)`;
+                fourthTd.textContent = (yearsFromStartStudy > 5 || yearsFromStartStudy == 4 && dateNow.getMonth > 10) ?
+                "закончил" :
+                `${array[i - 1].startStudyYear}-${dateNow.getFullYear()} (${yearsFromStartStudy} курс)`;
+                
                 tr.append(th);
                 tr.append(firstTd);
                 tr.append(secondTd);
+                tr.append(thirdTd);
+                tr.append(fourthTd);
                 tbody.append(tr);
             }
-
-            this.numberOfStudent++;
         }   
     };
     
@@ -60,5 +67,5 @@
 
     let buttonFormCreate = document.querySelector(".btn-primary");
     buttonFormCreate.addEventListener("click", createDataBase.addStudent.bind(createDataBase)); // привязываем функцию с созданием ученика, с учётом объекта createDataBase
-
+    buttonFormCreate.addEventListener("click", createDataBase.clearForm.bind(createDataBase)); // привязываем функцию с очищением формы, с учётом объекта createDataBase
 })();
