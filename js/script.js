651e6802d8d62941452c2174475f10299cb21dcc
Nick <84956743974@mail.ru>
2022-10-19 17:48:25 +0300

    add students, start validate form

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@
     const createDataBase = {
         arrayOfObjectsWithStudents: [], // глобальный массив, содержащий объекты всех студентов
 
+        check() {
+            console.log("check");
+        },
+
         clearForm(but) {
             but.preventDefault();
             let allInputs = document.querySelectorAll(".form-control");
@@ -18,7 +22,7 @@
             nowObject.surname = allInputs[0].value;
             nowObject.name = allInputs[1].value;
             nowObject.lastname = allInputs[2].value;
-            nowObject.birthDate = new Date(allInputs[3].value);
+            nowObject.birthDate = new Date(allInputs[3].value); // в свойство birthDate записываем объект с введённой датой
             nowObject.startStudyYear = allInputs[4].value;
             nowObject.facultet = allInputs[5].value;
             
@@ -28,44 +32,62 @@
 
         drawTableWithStudents(array) {
             let tbody = document.getElementById("tbody");
-            tbody.innerHTML = '';
+            tbody.innerHTML = ''; // очищаем тело таблицы
 
-            for (let i = 1; i < array.length + 1; i++) {
-                let tr = document.createElement("tr");
-                let th = document.createElement("th");
-                let firstTd = document.createElement("td");
-                let secondTd = document.createElement("td");
-                let thirdTd = document.createElement("td");
+            for (let i = 1; i < array.length + 1; i++) { // цикл с перебором всех значений в переданном в функцию массиве
+                // создание элементов таблицы
+                let trOfTable = document.createElement("tr");
+                let thOfTable = document.createElement("th");
+                let fioTd = document.createElement("td");
+                let facultetTd = document.createElement("td");
+                let birthDateTd = document.createElement("td");
                 let fourthTd = document.createElement("td");
-                let dateNow = new Date();
-                let yearsFromStartStudy = dateNow.getFullYear() - array[i - 1].startStudyYear;
-                let age = Math.trunc((dateNow.getTime() - array[i - 1].birthDate.getTime()) / 1000 / 60 / 60 / 24 / 365);
+
+                let dateNow = new Date(); // получаем дату в текущий момент
+                let yearsFromStartStudy = dateNow.getFullYear() - array[i - 1].startStudyYear; // получаем, сколько лет мы учимся, вычитая из текущего года год поступления
+                let ageOfStudent = Math.trunc((dateNow.getTime() - array[i - 1].birthDate.getTime()) / 1000 / 60 / 60 / 24 / 365); // получаем полный возраст ученика
 
                 
-                th.textContent = i;
-                th.setAttribute("scope", "row");
+                thOfTable.textContent = i; // id каждого ученика
+                thOfTable.setAttribute("scope", "row"); // добавляем атрибут для айди 
 
-                firstTd.textContent = array[i - 1].surname + " " + array[i - 1].name + " " + array[i - 1].lastname;
-                secondTd.textContent = array[i - 1].facultet;
-                thirdTd.textContent = `${array[i - 1].birthDate.getDate()}.${array[i - 1].birthDate.getMonth() + 1}.${array[i - 1].birthDate.getFullYear()} (${age} лет)`;
-                fourthTd.textContent = (yearsFromStartStudy > 5 || yearsFromStartStudy == 4 && dateNow.getMonth > 10) ?
-                "закончил" :
-                `${array[i - 1].startStudyYear}-${dateNow.getFullYear()} (${yearsFromStartStudy} курс)`;
+                fioTd.textContent = array[i - 1].surname + " " + array[i - 1].name + " " + array[i - 1].lastname; // полное ФИО ученика
+                facultetTd.textContent = array[i - 1].facultet; // факультет ученика
+                birthDateTd.textContent = `${array[i - 1].birthDate.getDate()}.${array[i - 1].birthDate.getMonth() + 1}.${array[i - 1].birthDate.getFullYear()} (${ageOfStudent} лет)`; // дата рождения ученика с его возрастом
+                fourthTd.textContent = (yearsFromStartStudy > 4 || yearsFromStartStudy == 4 && dateNow.getMonth() > 9 || yearsFromStartStudy + 1 === 5) ?
+                `${array[i - 1].startStudyYear}-${Number(array[i - 1].startStudyYear) + 4} (закончил)` : // если ученик закончил институт, годы обучение и статус "закончил"
+                dateNow.getMonth() < 9 ?
+                `${array[i - 1].startStudyYear}-${Number(array[i - 1].startStudyYear) + 4} (${yearsFromStartStudy} курс)` : // если ученик не перешёл на следующий курс
+                `${array[i - 1].startStudyYear}-${Number(array[i - 1].startStudyYear) + 4} (${yearsFromStartStudy + 1} курс)`; // если ученик перешёл на следующий курс
                 
-                tr.append(th);
-                tr.append(firstTd);
-                tr.append(secondTd);
-                tr.append(thirdTd);
-                tr.append(fourthTd);
-                tbody.append(tr);
+                trOfTable.append(thOfTable, fioTd, facultetTd, birthDateTd, fourthTd); // добавляем элементы таблицы в tr
+                tbody.append(trOfTable); // добавляем tr в тело таблицы
             }
-        }   
+        },
+
+        activateButtons() {
+            let buttonFormClear = document.querySelector(".btn-danger");
+            let buttonFormCreate = document.querySelector(".btn-primary");
+            let studentsForm = document.querySelector(".form");
+
+            buttonFormClear.addEventListener("click", createDataBase.clearForm.bind(createDataBase)); // привязываем функцию с очищением формы, с учётом объекта createDataBase
+            // buttonFormCreate.addEventListener("click", createDataBase.addStudent.bind(createDataBase)); // привязываем функцию с созданием ученика, с учётом объекта createDataBase
+            // buttonFormCreate.addEventListener("click", createDataBase.clearForm.bind(createDataBase)); // привязываем функцию с очищением формы, с учётом объекта createDataBase
+            studentsForm.addEventListener('submit', function(event) {
+                console.log("submit");
+                
+                event.preventDefault();
+                let studentsForm = document.querySelector(".form");
+                let studentsFormInputs = document.querySelectorAll(".form-control");
+                let studentsFormLabels = document.querySelectorAll(".start__title");
+                let spanElements = document.querySelectorAll(".start__error");
+                return false;
+            });
+
+            
+            studentsForm.addEventListener('submit', createDataBase.addStudent.bind(createDataBase));
+        }
     };
     
-    let buttonFormClear = document.querySelector(".btn-danger");
-    buttonFormClear.addEventListener("click", createDataBase.clearForm.bind(createDataBase)); // привязываем функцию с очищением формы, с учётом объекта createDataBase
-
-    let buttonFormCreate = document.querySelector(".btn-primary");
-    buttonFormCreate.addEventListener("click", createDataBase.addStudent.bind(createDataBase)); // привязываем функцию с созданием ученика, с учётом объекта createDataBase
-    buttonFormCreate.addEventListener("click", createDataBase.clearForm.bind(createDataBase)); // привязываем функцию с очищением формы, с учётом объекта createDataBase
+    createDataBase.activateButtons();
 })();
